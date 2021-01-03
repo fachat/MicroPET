131dbc71d259fe1c50f09a24e8070bd28e07a97d
A. Fachat <afachat@gmx.de>
2021-01-03 20:16:29 +0100

    fixed VGA timing issues; implement 9 row chars in VGA

--- CPLD/VideoVGA.vhd
+++ CPLD/VideoVGA.vhd
@@ -82,7 +82,7 @@ architecture Behavioral of Video is
 	signal interlace : std_logic;
 	
 	-- hold and shift the pixel
-	signal pxlhold : std_logic_vector (7 downto 0) := (others => '0');
+	signal pxlhold : std_logic_vector (11 downto 0) := (others => '0');
 	-- hold the character information
 	signal charhold : std_logic_vector (7 downto 0) := (others => '0');
 
@@ -103,11 +103,12 @@ architecture Behavioral of Video is
 	
 	-- geo signals
 	--
-	-- pulse at end of raster line
-	signal last_slot_of_line : std_logic := '0';
-	-- pulse for last visible character/slot
+	-- pulse at end of raster line; falling slotclk
+	signal last_slot_of_line : std_logic := '0'; 
+	signal last_slot_of_line_d : std_logic := '0';
+	-- pulse for last visible character/slot; falling slotclk
 	signal last_vis_slot_of_line : std_logic := '0';
-	-- pulse at end of character line 
+	-- pulse at end of character line; falling slotclk
 	signal last_line_of_char : std_logic := '0';
 	-- pulse at end of screen
 	signal last_line_of_screen : std_logic := '0';
@@ -151,7 +152,7 @@ begin
 		if (reset = '1') then
 			in_slot <= '0';
 		elsif (falling_edge(slotclk)) then
-			in_slot <= not(in_slot);
+			in_slot <= slot_cnt(0);
 		end if;
 	end process;
 	
@@ -160,10 +161,10 @@ begin
 	-- pxl40/chr40 are used in both 40 and 80 col mode
 	-- pxl40/80 must be active at last cycle before loading the pixel shift register
 	-- 	as the pixel SR is directly loaded from the data bus
-	chr40 <= chr_window and in_slot 	and not(is_hires);
-	pxl40 <= pxl_window and in_slot;
-	chr80 <= chr_window and not(in_slot) 	and not(is_hires) 	and is_80;
-	pxl80 <= pxl_window and not(in_slot)		  		and is_80;
+	chr40 <= chr_window and in_slot 	and not(is_hires)	and is_80;
+	pxl40 <= pxl_window and in_slot					and is_80;
+	chr80 <= chr_window and not(in_slot) 	and not(is_hires);
+	pxl80 <= pxl_window and not(in_slot);
 
 	-- note: at least pxl_fetch is used in loading the video shift register, at falling edge of a clock
 	-- so the combinatorial part will glitch, and sometimes not fulfill the condition to load the SR.
@@ -216,7 +217,7 @@ begin
 			end if;
 			
 			-- sync
-			if (slot_cnt >= 83 and slot_cnt <= 95) then
+			if (slot_cnt >= 82 and slot_cnt <= 94) then
 				h_sync_int <= '1';
 			else
 				h_sync_int <= '0';
@@ -240,6 +241,13 @@ begin
 		end if;
 	end process;
 
+	last_slot_p: process(qclk, last_slot_of_line)
+	begin
+		if (rising_edge(qclk)) then
+			last_slot_of_line_d <= last_slot_of_line;
+		end if;
+	end process;
+	
 	h_sync <= not(h_sync_int); -- and not(v_sync_int));
 	
 	-----------------------------------------------------------------------------
@@ -265,10 +273,18 @@ begin
 				end if;
 			end if;
 			
-			if (rline_cnt >= 450 and rline_cnt < 452) then
-				v_sync_int <= '1';
+			if (is_9rows = '1' or is_10rows = '1') then
+				if (rline_cnt >= 467 and rline_cnt < 469) then
+					v_sync_int <= '1';
+				else
+					v_sync_int <= '0';
+				end if;
 			else
-				v_sync_int <= '0';
+				if (rline_cnt >= 450 and rline_cnt < 452) then
+					v_sync_int <= '1';
+				else
+					v_sync_int <= '0';
+				end if;
 			end if;
 		end if;
 	end process;
@@ -277,53 +293,23 @@ begin
 	begin
 		if (falling_edge(h_sync_int)) then
 			
---		    if (is_9rows = '1') then
---			-- timing for 9 pixel rows per character
---			-- end of character line
---			if (is_hires = '1' or cline_cnt = 8) then
---				-- if hires, everyone
---				last_line_of_char <= '1';
---			else
---				last_line_of_char <= '0';
---			end if;
---				
---			-- vsync
---			if (rline_cnt >= 265 and rline_cnt < 281) then
---				v_sync_int <= '1';
---			else
---				v_sync_int <= '0';
---			end if;
---			
---			-- venable
---			if (rline_cnt < 225) then
---				v_enable <= '1';
---			else
---				v_enable <= '0';
---			end if;
---		    elsif (is_10rows = '1') then
---			-- timing for 10 pixel rows per character
---			-- end of character line
---			if (is_hires = '1' or cline_cnt = 9) then
---				-- if hires, everyone
---				last_line_of_char <= '1';
---			else
---				last_line_of_char <= '0';
---			end if;
---				
---			-- vsync
---			if (rline_cnt >= 275 and rline_cnt < 291) then
---				v_sync_int <= '1';
---			else
---				v_sync_int <= '0';
---			end if;
---			
---			-- venable
---			if (rline_cnt < 250) then
---				v_enable <= '1';
---			else
---				v_enable <= '0';
---			end if;
---		    else
+		    if (is_9rows = '1' or is_10rows = '1') then
+			-- timing for 9 pixel rows per character
+			-- end of character line
+			if ((is_hires = '1' or cline_cnt = 8) and rline_cnt(0) = '1') then
+				-- if hires, everyone
+				last_line_of_char <= '1';
+			else
+				last_line_of_char <= '0';
+			end if;
+
+			-- venable
+			if (rline_cnt < 450) then
+				v_enable <= '1';
+			else
+				v_enable <= '0';
+			end if;
+		    else
 			-- timing for 8 pixel rows per character
 			-- end of character line
 			if ((is_hires = '1' or cline_cnt = 7) and rline_cnt(0) = '1') then
@@ -332,23 +318,14 @@ begin
 			else
 				last_line_of_char <= '0';
 			end if;
-			
-			-- vsync
-			--if (rline_cnt >= 490 and rline_cnt < 492) then
-			-- center the 400 lines we do in the 480 available lines
---			if (rline_cnt >= 450 and rline_cnt < 452) then
---				v_sync_int <= '1';
---			else
---				v_sync_int <= '0';
---			end if;
-			
+						
 			-- venable
 			if (rline_cnt < 400) then
 				v_enable <= '1';
 			else
 				v_enable <= '0';
 			end if;
---		    end if; -- crtc_is_9rows
+		    end if; -- crtc_is_9rows
 
 		    -- common for 8/9 pixel rows per char
 		    
@@ -463,9 +440,13 @@ begin
 		elsif (falling_edge(dotclk)) then
 			-- dotclk falls at same edge as memclk (falling qclk)
 			-- note: pxl_fetch is registered with qclk above, as is memclk_d (rising qclk)
+			pxlhold(11) <= pxlhold(10);
+			pxlhold(10) <= pxlhold(9);
+			pxlhold(9) <= pxlhold(8);
+			pxlhold(8) <= pxlhold(7) and enable;
 			if (pxl_fetch = '1' and sr_load_d ='1') then
 				enable <= h_enable and v_enable;
-				pxlhold <= D;
+				pxlhold(7 downto 0) <= D;
 			elsif (dot2clk_d = '1' or is_80 = '1') then 
 				pxlhold(7) <= pxlhold(6);
 				pxlhold(6) <= pxlhold(5);
@@ -478,8 +459,8 @@ begin
 			end if;
 		end if;
 	end process;
-	
-	pxl_out <= (pxlhold(7)) and enable;
+
+	pxl_out <= (pxlhold(8));-- and enable;
 
 	--------------------------------------------
 	-- crtc register emulation
